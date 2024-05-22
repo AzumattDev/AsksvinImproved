1dc41fd5b163aa0768630d5d34b20cb19ae8e6e2
AzumattDev <azumattgaming@gmail.com>
2024-05-22 05:51:03 -0400

    v1.0.7, Pins

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ namespace AsksvinImproved
     public class AsksvinImprovedPlugin : BaseUnityPlugin
     {
         internal const string ModName = "AsksvinImproved";
-        internal const string ModVersion = "1.0.6";
+        internal const string ModVersion = "1.0.7";
         internal const string Author = "Azumatt";
         private const string ModGUID = $"{Author}.{ModName}";
         private readonly Harmony _harmony = new(ModGUID);
@@ -25,11 +25,104 @@ namespace AsksvinImproved
         }
     }
 
+    /*[HarmonyPatch(typeof(Player), nameof(Player.FixedUpdate))]
+    public static class FixedUpdatePatch
+    {
+        public static void Postfix(ref Player __instance)
+        {
+            if (__instance != Player.m_localPlayer || !__instance.TakeInput()) return;
+            bool keyPressed = false;
+
+            //Do the Get Creatures.
+            List<Character> guysList = Character.GetAllCharacters();
+            EnemyHud.instance.m_refPoint = __instance.transform.position;
+            int guysNum = 0;
+
+
+                foreach (Character character in guysList.Where(character => character is not Player && EnemyHud.instance.TestShow(character, false)))
+                {
+                    guysNum++;
+                    EnemyHud.instance.ShowHud(character, false);
+                    EnemyHud.instance.m_huds.TryGetValue(character, out EnemyHud.HudData hud);
+                    if (hud == null) continue;
+                    hud.m_hoverTimer = 0F;
+                    hud.m_gui.SetActive(true);
+                }
+
+
+            //Update the enemy huds list if we found anything
+            if (guysNum > 0)
+            {
+                Sadle? sadle = null;
+                EnemyHud.instance.UpdateHuds(__instance, sadle, Time.deltaTime);
+            }
+        }
+    }*/
+
+    [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdateEventPin))]
+    public static class UpdateEventPinPatch
+    {
+        public static void Postfix(Minimap __instance)
+        {
+            if (PlayerStartDoodadControlPatch.LastHumanoidZDOID.IsNone()) return;
+            //Populate the list of current HUD characters.
+            List<Character> guysList =
+                (from hud
+                        in EnemyHud.instance.m_huds.Values
+                    where hud.m_character != null
+                          && hud.m_character.IsTamed()
+                          && hud.m_character.GetZDOID() == PlayerStartDoodadControlPatch.LastHumanoidZDOID
+                    select hud.m_character
+                ).ToList();
+            //Add minimap pins if they haven't been added already.
+            foreach (Character character
+                     in from character in guysList
+                     where character is not Player
+                     let flag = __instance.m_pins.Any(pin => pin.m_name.Equals($"$hud_tame {character.GetHoverName()} [Health: {character.GetHealth()}]"))
+                     where !flag
+                     select character)
+            {
+                __instance.AddPin(character.GetCenterPoint(), Minimap.PinType.None, $"$hud_tame {character.GetHoverName()} [Health: {character.GetHealth()}]", false, false);
+                Sadle? sadle = null;
+                EnemyHud.instance.UpdateHuds(Player.m_localPlayer, sadle, Time.deltaTime);
+            }
+
+            //Remove minimap pins which are not needed anymore.
+            List<Minimap.PinData> removePins = new();
+
+            foreach (Minimap.PinData pin in __instance.m_pins)
+            {
+                if (pin.m_type != Minimap.PinType.None) continue;
+                bool flag = false;
+                foreach (Character character in guysList.Where(character => pin.m_name.Equals($"$hud_tame {character.GetHoverName()} [Health: {character.GetHealth()}]")))
+                {
+                    pin.m_pos.x = character.GetCenterPoint().x;
+                    pin.m_pos.y = character.GetCenterPoint().y;
+                    pin.m_pos.z = character.GetCenterPoint().z;
+                    flag = true;
+                    break;
+                }
+
+                if (!flag)
+                {
+                    removePins.Add(pin);
+                }
+            }
+
+            foreach (Minimap.PinData pin in removePins)
+            {
+                __instance.RemovePin(pin);
+                AsksvinImprovedPlugin.AsksvinImprovedLogger.LogDebug("removing pin for " + pin.m_name);
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(Player), nameof(Player.StartDoodadControl))]
     static class PlayerStartDoodadControlPatch
     {
         public static bool RidingAsksvin;
         public static Humanoid RidingHumanoid = null!;
+        public static ZDOID LastHumanoidZDOID;
 
         static void Postfix(Player __instance, IDoodadController shipControl)
         {
@@ -40,6 +133,7 @@ namespace AsksvinImproved
             {
                 RidingAsksvin = true;
                 RidingHumanoid = shipControl.GetControlledComponent().transform.GetComponentInParent<Humanoid>();
+                LastHumanoidZDOID = RidingHumanoid.GetZDOID();
             }
         }
     }
--- Thunderstore/CHANGELOG.md
+++ Thunderstore/CHANGELOG.md
@@ -1,9 +1,10 @@
-| `Version` | `Update Notes`                                                        |
-|-----------|-----------------------------------------------------------------------|
-| 1.0.6     | - Each attack uses 5% of max stamina to help balance.                 |
-| 1.0.5     | - Dynamically check for Jump key.                                     |
-| 1.0.4     | - No update needed, just bumping the time to be after Ashlands.       |
-| 1.0.3     | - Controller support that was missing (for the attacks).              |
-| 1.0.2     | - Fix manifest.json description to reflect removal of code from 1.0.1 |
-| 1.0.1     | - Remove the Lava toleration code. It's now in the base game.         |
-| 1.0.0     | - Initial Release                                                     |
+| `Version` | `Update Notes`                                                                                                                                                                                                    |
+|-----------|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| 1.0.7     | - If you're close to a recently dismounted Askvin, they will display as a pin on your minimap. Pin will move to a new Askvin, should you mount another. Pins are not persistent and do not save between sessions. |
+| 1.0.6     | - Each attack uses 5% of max stamina to help balance.                                                                                                                                                             |
+| 1.0.5     | - Dynamically check for Jump key.                                                                                                                                                                                 |
+| 1.0.4     | - No update needed, just bumping the time to be after Ashlands.                                                                                                                                                   |
+| 1.0.3     | - Controller support that was missing (for the attacks).                                                                                                                                                          |
+| 1.0.2     | - Fix manifest.json description to reflect removal of code from 1.0.1                                                                                                                                             |
+| 1.0.1     | - Remove the Lava toleration code. It's now in the base game.                                                                                                                                                     |
+| 1.0.0     | - Initial Release                                                                                                                                                                                                 |
