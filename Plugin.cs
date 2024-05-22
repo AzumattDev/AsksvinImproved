358b6b967ea973457bd58cfeeb911ca3da42b5fd
AzumattDev <azumattgaming@gmail.com>
2024-05-22 05:53:59 -0400

    Remove unused code

--- Plugin.cs
+++ Plugin.cs
@@ -25,40 +25,6 @@ namespace AsksvinImproved
         }
     }
 
-    /*[HarmonyPatch(typeof(Player), nameof(Player.FixedUpdate))]
-    public static class FixedUpdatePatch
-    {
-        public static void Postfix(ref Player __instance)
-        {
-            if (__instance != Player.m_localPlayer || !__instance.TakeInput()) return;
-            bool keyPressed = false;
-
-            //Do the Get Creatures.
-            List<Character> guysList = Character.GetAllCharacters();
-            EnemyHud.instance.m_refPoint = __instance.transform.position;
-            int guysNum = 0;
-
-
-                foreach (Character character in guysList.Where(character => character is not Player && EnemyHud.instance.TestShow(character, false)))
-                {
-                    guysNum++;
-                    EnemyHud.instance.ShowHud(character, false);
-                    EnemyHud.instance.m_huds.TryGetValue(character, out EnemyHud.HudData hud);
-                    if (hud == null) continue;
-                    hud.m_hoverTimer = 0F;
-                    hud.m_gui.SetActive(true);
-                }
-
-
-            //Update the enemy huds list if we found anything
-            if (guysNum > 0)
-            {
-                Sadle? sadle = null;
-                EnemyHud.instance.UpdateHuds(__instance, sadle, Time.deltaTime);
-            }
-        }
-    }*/
-
     [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdateEventPin))]
     public static class UpdateEventPinPatch
     {
